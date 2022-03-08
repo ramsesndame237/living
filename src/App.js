8cb4e05b570a10d34de5ffea92acbd85e6928dd0
Ramses ndame <64875585+ramsesndame237@users.noreply.github.com>
2022-03-08 18:23:13 +0100

    get all movies and display it

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ function App() {
     <h1>
       Hey gar we are building our first react movies app basing on netflix film
     </h1>
-    <Row title="NEXTFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+    <Row title="NEXTFLIX ORIGINALS" isLargeRow={true} fetchUrl={requests.fetchNetflixOriginals} />
     <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+    <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+    <Row title="Action Movies" fetchUrl={requests.fethActionsMovies} />
+    <Row title="Comedy Movies" fetchUrl={requests.fethComdedyMovies} />
+    <Row title="Horror Movies" fetchUrl={requests.fethHorrorMovies} />
+    <Row title="Romance Movies" fetchUrl={requests.fethRomanceMovies} />
+    <Row title="Documentaries" fetchUrl={requests.fethDocumentariesMovies} />
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from "../services/axios"
 
-function Row({title,fetchUrl}){
+import "./Row.css"
+
+const baseURL ="https://image.tmdb.org/t/p/original/"
+
+function Row({title,fetchUrl, isLargeRow}){
 
     const [movies, SetMovies] = useState([])
 
@@ -22,7 +26,12 @@ function Row({title,fetchUrl}){
             <h2>
                 {title}
             </h2>
-            <div className='row__posters'>
+            <div className='row__posters'> 
+            {
+                movies.map(movie =>( 
+                    <img  key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} alt={movie.name} />
+                ))
+            }
 
             </div>
 
